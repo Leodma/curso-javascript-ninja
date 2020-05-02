307d28f28b45432274963a31fa0488dbb4a11fe9
Leo <leodma@gmail.com>
2020-05-02 18:49:09 -0300

    Resolução desafio 21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -14,4 +14,64 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+(function(win, doc){
+    'use strict'
+
+    var counterDislpay = doc.querySelector('[data-js="contador"]');
+    var btStart = doc.querySelector('[data-js = "btStart"]');
+    var btStop = doc.querySelector('[data-js = "btStop"]');
+    var btRestart = doc.querySelector('[data-js ="btRestart"]');
+
+ 
+    
+    var milisseconds = 0;
+    var seconds = 0;
+    var minutes = 0;
+    var hours = 0;
+    var timer;
+   
+
+    function startCounter( ){
+
+        if (milisseconds === 100){
+            milisseconds = 0;
+            seconds++;
+
+        };
+        if (seconds === 60){
+            seconds = 0;
+            minutes++;
+        };
+
+        if (minutes === 60){
+            minutes = 0;
+            hours++;
+        };
+
+        counterDislpay.innerHTML = hours + ':' +  minutes + ':' + seconds + ':' + milisseconds;
+        milisseconds ++;
+        return timer = setTimeout(startCounter, 1);
+    };
+
+    function stopCounter(){
+        return clearTimeout(timer);
+    };
+
+    function restartCounter(){
+        seconds = 0;
+        milisseconds = 0;
+        minutes = 0;
+        hours = 0;
+        counterDislpay.innerHTML = hours + ':' +  minutes + ':' + seconds + ':' + milisseconds;   
+        return stopCounter();
+    };
+   
+    btStart.addEventListener('click', startCounter, false);
+
+    btStop.addEventListener('click', stopCounter, false);
+
+    btRestart.addEventListener('click', restartCounter, false);
+
+
+
+})(window, document);
