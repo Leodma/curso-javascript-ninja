81cf9a8160ed93b828c42597a1d6db6d62e5a8e7
Leo <leodma@gmail.com>
2020-06-12 09:23:32 -0300

    Pull Request do desafio 31

--- challenge-31/challenge-31.js
+++ challenge-31/challenge-31.js
@@ -11,4 +11,4 @@ Faça um pull request, também com a branch `challenge-31`, mas no repositório
 do curso, para colar o link do pull request do seu repo.
 */
 
-console.log('Link do pull request do seu projeto');
+console.log('https://github.com/Leodma/cadastro-carros/pull/2/commits/7f30a510881571a5b1d5bfcd716eb3508eda9fc3');
